a4d1bd57fedd11b116b95f88ae02a67a410d3fb6
mohdfahoum <mohd.fahoum@gmail.com>
2020-06-24 15:50:02 +0300

    loops

--- js/index1.js
+++ js/index1.js
@@ -2,21 +2,29 @@
 
 function bearType() {
   var bear = prompt("Please enter the bear name you want to ask about");
-  
-  if(bear != null){
-    document.write("please reload the page and type the bear you want ");
-  }
-
 
   var bearType;
-  var asian = "asian";
-  var russian  = "russian";
-  if (bear === asian) {
-    bearType = 'Correct answer ';
-  } else if (bear === russian) {
-    bearType = 'worng answer! ';
+  var asian = 'asian';
+  var russian = 'russian';
+
+  while (bear !== 'asian' && bear !== 'russian') {
+    bear = prompt("Please enter the bear name you want to ask about");
   }
-  document.write('<h3>' + bearType + '</h3>');
+
+
+  
+
+  var howMany = prompt('please type how many bears you want?')
+
+  for(var i = 1; i <= howMany; i++) {
+
+    if (bear == asian) {
+      bear = 'Correct answer ';
+    } else if (bear == russian) {
+      bear = 'worng answer! ';
+    }
+    document.write('<h3>' + bear + '</h3>');
+  }
 }
 
 bearType();
